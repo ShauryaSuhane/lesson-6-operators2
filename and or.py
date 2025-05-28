e9d216f9f1765a13bac8c6feff0b7b27e611ea76
ShauryaSuhane <suhaneshaurya26@gmail.com>
2025-05-28 10:04:42 +0530

    and or

---
+++ and or.py
@@ -0,0 +1,13 @@
+a=9
+b=5
+c=0
+if a and b and c:
+    print("all of the numbers are true")
+else:
+    print("one of them is false")
+a=13
+b=7
+if a>b or b>a:
+    print("all the numbers are true")
+else:
+    print("one of them is false")
