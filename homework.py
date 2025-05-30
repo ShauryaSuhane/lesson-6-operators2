69ddf0c261ca874da5c8f9cccc644c273583dbf3
ShauryaSuhane <suhaneshaurya26@gmail.com>
2025-05-30 09:20:22 +0530

    homework

---
+++ homework.py
@@ -0,0 +1,5 @@
+character=(input("enter the character:"))
+if (character>="A" and character<="Z") or (character>="a" and character<="z"):
+    print("it is a character")
+else:
+    print("it is not a character")
